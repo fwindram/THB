50f4945babf64a6e3da97d0a37e94e7f87862758
Francis Windram <francis windram>
2017-04-23 22:52:30 +0100

    Implemented logging to file.

--- THB.py
+++ THB.py
@@ -12,13 +12,20 @@ import time
 import os
 
 # Set up logging
+# logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# Create logfile handler
 handler = logging.FileHandler('THB.out')
 handler.setLevel(logging.DEBUG)
 
+# Create formatter and add to handler
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
 
+# Add handler to logger
+logger.addHandler(handler)
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("AskReddit")
@@ -26,8 +33,19 @@ threads_to_pull = 20
 
 
 def get_newthreads(threads=20):
+    logger.debug("Getting newest {0} submissions to /r/AskReddit".format(threads))
     for submission in subreddit.new(limit=threads):
-        print("Title: {0}".format(submission.title))
-        print("Score: {0}".format(submission.score))
-        print("Comments: {0}".format(submission.num_comments))
-        print()
+        logger.debug("Found thread ID {0}".format(submission.id))
+        # print("Title: {0}".format(submission.title))
+        # print("Score: {0}".format(submission.score))
+        # print("Comments: {0}".format(submission.num_comments))
+
+
+
+def logtest():
+    logger.debug("DEBUG TEST")
+    logger.info("INFO TEST")
+    logger.warning("WARNING TEST")
+    logger.error("ERROR TEST")
+    logger.critical("CRITICAL TEST")
+    logger.fatal("FATAL TEST")
