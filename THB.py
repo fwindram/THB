0748006cb07a8bcc9e16835adfc6d0dd8e2aab11
Francis Windram <francis windram>
2017-04-27 17:35:05 +0100

    Implemented performance logging

--- THB.py
+++ THB.py
@@ -20,7 +20,7 @@ logger.setLevel(logging.DEBUG)
 
 # Create logfile handler
 handler = logging.FileHandler('log/THB.out')
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.DEBUG)     # File logging level
 
 # Create formatter and add to handler
 formatter = logging.Formatter('%(asctime)s - %(levelname)8s - %(funcName)s - %(message)s')
@@ -31,13 +31,12 @@ logger.addHandler(handler)
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("AskReddit")
-threads_to_pull = 20
+threads_to_pull = 20        # Could define dynamically
 
 
 def readfiles():
     """
-    reads the current working list into memory
-    :return: watchedthreads{}
+    Read the current working file and return as a dict
     """
     watchedthreads = {}
     try:
@@ -46,14 +45,15 @@ def readfiles():
             for submission in watchedreader:
                 # Split submission into 'id':('created_utc', ['score1|posts1', 'score2|posts2'...]
                 watchedthreads[submission[0]] = (float(submission[1]), submission[2:len(submission)+1])
+                logger.debug("Loaded {0}".format(submission[0]))
             # pprint.pprint(watchedthreads)
-            logger.info("Watching {0} threads.".format(len(watchedthreads)))
     except FileNotFoundError:
         logger.warning("No watchlist file present. Using empty watchlist.")
+    logger.info("Watching {0} threads.".format(len(watchedthreads)))
     return watchedthreads
 
 
-def get_newthreads(threads=2):
+def get_newthreads(watchedthreads, threads=2):
     logger.debug("Getting newest {0} submissions to /r/AskReddit".format(threads))
     for submission in subreddit.new(limit=threads):
         logger.debug("Found thread ID {0}".format(submission.id))
@@ -62,16 +62,16 @@ def get_newthreads(threads=2):
         # print("Comments: {0}".format(submission.num_comments))
 
 
-def logtest():
-    logger.debug("DEBUG TEST")
-    logger.info("INFO TEST")
-    logger.warning("WARNING TEST")
-    logger.error("ERROR TEST")
-    logger.critical("CRITICAL TEST")
-    logger.fatal("FATAL TEST")
-
-
 def main():
+    starttime = time.perf_counter()
+    logger.info("-----------------------------------------")
+    logger.info("Started execution at {0}".format(time.strftime("%H:%M:%S, %d/%m/%Y", time.localtime())))
+    logger.info("-----------------------------------------")
     watched = readfiles()
-
+    endtime = time.perf_counter()
+    mins, secs = divmod(endtime - starttime, 60)    # Easiest way to quickly split into mm:ss
+    logger.info("-----------------------------------------")
+    logger.info("Ended execution at {0}".format(time.strftime("%H:%M:%S, %d/%m/%Y", time.localtime())))
+    logger.info("Executed in {0}m, {1}s.".format(int(mins), secs))
+    logger.info("-----------------------------------------")
 main()
