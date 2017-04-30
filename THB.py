085efb19432e3866235582af62da9261c12f15c7
Francis Windram <francis windram>
2017-04-30 19:56:08 +0100

    Added submission-pulling

--- THB.py
+++ THB.py
@@ -20,7 +20,7 @@ logger.setLevel(logging.DEBUG)
 
 # Create logfile handler
 handler = logging.FileHandler('log/THB.out')
-handler.setLevel(logging.DEBUG)     # File logging level
+handler.setLevel(logging.DEBUG)  # File logging level
 
 # Create formatter and add to handler
 formatter = logging.Formatter('%(asctime)s - %(levelname)8s - %(funcName)s - %(message)s')
@@ -31,8 +31,9 @@ logger.addHandler(handler)
 
 reddit = praw.Reddit('bot1')
 subreddit = reddit.subreddit("AskReddit")
-threads_to_pull = 20        # Could define dynamically
-archiveage = 86400
+base_threads_to_pull = 5  # Calculated that over a 1/10m every 24h (288 runs) this should never hit 500
+maxthreads = 500
+archiveage = 86400  # 24h = 86400s
 
 
 def readfiles():
@@ -44,28 +45,38 @@ def readfiles():
         with open("data/watched.csv") as watchedfile:
             watchedreader = csv.reader(watchedfile)
             for submission in watchedreader:
-                # Split submission into 'id':('created_utc', ['score1|posts1', 'score2|posts2'...]
-                watchedthreads[submission[0]] = (float(submission[1]), submission[2:len(submission)+1])
-                logger.debug("Loaded {0}".format(submission[0]))
-            # pprint.pprint(watchedthreads)
+                # Split submission into 'id':('created_utc', ['offset|score1|posts1', 'offset|score2|posts2'...]
+                watchedthreads[submission[0]] = (float(submission[1]), submission[2:len(submission) + 1])
+                logger.debug("Loaded {0}.".format(submission[0]))
     except FileNotFoundError:
         logger.warning("No watchlist file present. Using empty watchlist.")
 
-    logger.info("Watching {0} threads.".format(len(watchedthreads)))
+    logger.info("Pulled {0} threads from watchlist.".format(len(watchedthreads)))
     return watchedthreads
 
 
-def get_threads(watchedthreads, threads=2):
-    logger.debug("Getting newest {0} submissions to /r/AskReddit".format(threads))
+def get_threads(watchedthreads, threads=1):
+    logger.debug("Getting newest {0} submissions to /r/AskReddit.".format(threads))
+    added = 0
     for submission in subreddit.new(limit=threads):
-        logger.debug("Found thread ID {0}".format(submission.id))
-        # print("Title: {0}".format(submission.title))
-        # print("Score: {0}".format(submission.score))
-        # print("Comments: {0}".format(submission.num_comments))
+        logger.debug("Found {0}.".format(submission.id))
+        if submission.id not in watchedthreads:
+            watchedthreads[submission.id] = (submission.created_utc,
+                                             ["{0}|{1}|{2}".format(int(time.time() - submission.created_utc),
+                                                                   submission.score,
+                                                                   submission.num_comments)
+                                              ]
+                                             )
+            logger.debug("Added {0} to watchlist.".format(submission.id))
+            added += 1
+        else:
+            logger.debug("{0} already in watchlist.".format(submission.id))
+    logger.info("Added {0}/{1} new threads to watchlist.".format(added, threads))
+    return watchedthreads
 
 
 def archive_threads(watchedthreads, stale_age=86400):
-    """Archive threads over 24h old."""
+    """Archive threads over stale_age old (usually 24h)."""
     # We archive threads before polling to decrease the number of requests made to the reddit API.
     # And so we can implement archiving without pulling new threads as an alternate execution mode.
     logger.debug("Archiving stale threads.")
@@ -73,25 +84,25 @@ def archive_threads(watchedthreads, stale_age=86400):
 
     for threadid in watchedthreads:
         threadvalues = watchedthreads[threadid]
-        age = time.time() - threadvalues[0]     # Calculate age from current time and created_utc
-        agemins, agesecs = divmod(age, 60)      # Cannot use strftime as time could be over 24h, which causes problems.
+        age = time.time() - threadvalues[0]  # Calculate age from current time and created_utc
+        agemins, agesecs = divmod(age, 60)  # Cannot use strftime as time could be over 24h, which causes problems.
         agehrs, agemins = divmod(agemins, 60)
         logger.debug("Thread {0} is {1}h {2}m {3}s old.".format(threadid, int(agehrs), int(agemins), int(agesecs)))
-        if age > stale_age:     # Stale age is 24h by default
+        if age > stale_age:  # Stale age is 24h by default
             logger.debug("Thread {0} is stale.".format(threadid))
             # Build archive row for entry
             archiverow = [threadid, threadvalues[0]]
             for timesegment in threadvalues[1]:
                 archiverow.append(timesegment)
             stalethreads.append(archiverow)
-        
-    with open("data/archive.csv", "a", newline='') as archivefile:      # Automatically creates file if not found
+
+    with open("data/archive.csv", "a", newline='') as archivefile:  # Automatically creates file if not found
         # newline behaviour will have to be checked on UNIX.
         archivewriter = csv.writer(archivefile)
         archivewriter.writerows(stalethreads)
         logger.info("Archived {0} stale threads.".format(len(stalethreads)))
     for thread in stalethreads:
-        del watchedthreads[thread[0]]    # Remove archived threads from watchedthreads
+        del watchedthreads[thread[0]]  # Remove archived threads from watchedthreads
     logger.debug("Deleted {0} stale threads from watchlist.".format(len(stalethreads)))
 
     return watchedthreads
@@ -104,10 +115,16 @@ def main():
     logger.info("-----------------------------------------")
     watched = readfiles()
     watched = archive_threads(watched, archiveage)
+    # Calculate threads to pull as a proportion of maxthreads, but always look to pull 1.
+    threads_to_pull = max(int((maxthreads - len(watched)) / maxthreads * base_threads_to_pull), 1)
+    if len(watched) + threads_to_pull <= maxthreads:  # Don't bother getting new threads if already at max.
+        watched = get_threads(watched, threads_to_pull)
     endtime = time.perf_counter()
     runtime = time.strftime("%H:%M:%S", time.gmtime(endtime - starttime))
     logger.info("-----------------------------------------")
     logger.info("Ended execution at {0}".format(time.strftime("%H:%M:%S, %d/%m/%Y", time.localtime())))
     logger.info("Executed in {0}.".format(runtime))
     logger.info("-----------------------------------------")
+
+
 main()
