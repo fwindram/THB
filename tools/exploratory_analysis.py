e75dad29e39680752939c13b83cb1980c509056b
Francis Windram <francis windram>
2017-06-16 13:58:53 +0100

    Added basic statistics analysis

--- tools/exploratory_analysis.py
+++ tools/exploratory_analysis.py
@@ -9,7 +9,7 @@
 # TODO: Generate basic statistics:
 #   - Sample size
 #   - Total data points
-#   - Range
+#   - Final score range
 #   - Median final score/comments
 #   - Mean & SD of same
 # TODO: Identify top posts
@@ -24,6 +24,29 @@
 #   - Maybe see https://plot.ly/python/line-charts/
 
 from pprint import pprint
+import csv
+import math
+import statistics
+from collections import namedtuple
+
+
+def read_archive():
+    """Read the current archive file and return as a dict"""
+    print("Reading archive.")
+
+    archivethreads = {}
+    try:
+        with open("../data/archive_binned.csv") as archivefile:
+            archivereader = csv.reader(archivefile)
+            for submission in archivereader:
+                # Split submission into 'id':('created_utc', ['offset|score1|posts1', 'offset|score2|posts2'...]
+                archivethreads[submission[0]] = (float(submission[1]), submission[2:len(submission) + 1])
+    except FileNotFoundError:
+        print("No binned archive file present. Using empty archive.")
+
+    print("Pulled {0} threads from archive.".format(len(archivethreads)))
+
+    return archivethreads
 
 
 def find_percentage_marks(seq_len, period=10, index_from=0):
@@ -68,13 +91,49 @@ def find_percentage_marks(seq_len, period=10, index_from=0):
         target_numbers[step_idx] = target_counter
 
     return target_numbers
-test = [x + 1 for x in range(3565)]
 
-marks = find_percentage_marks(3565)
-pprint(marks)
-for i in range(3565):
-    if i in marks:
-        print("{0}% at {1}".format(marks[i], test[i]))
 
-# for mark in marks:
-#     print("{0}% at {1}".format(marks[mark], test[mark]))
+def find_final_entry(archive):
+    """Returns all the final scores from an archive in the THB format.
+    
+    Using one (fairly obtuse) dictionary comprehension, this finds the final recorded score of each ID.
+    
+    :param archive: Archive of entries in the standard THB format.
+    :type archive: dict
+    :returns: namedtuple containing (full list of IDs and associated data, Hi->Lo sorted scores, Hi->Lo sorted comments)
+    """
+    # Find the final entry in the entries list for each ID, split on |, and get comments & score
+    full = [[x, int(archive[x][1][-1].split('|')[1]), int(archive[x][1][-1].split('|')[2])] for x in archive]
+    scores = [x[1] for x in full]
+    comments = [x[2] for x in full]
+    scores.sort(reverse=True)
+    comments.sort(reverse=True)
+    out_tuple = namedtuple("final", "full, score, comments")    # We use a named tuple to allow access by name.
+    output = out_tuple(full, scores, comments)
+    return output
+
+
+def basic_statistics(sorted_arc):
+
+    sample_size = (len(sorted_arc))
+    sample_max = sorted_arc[0]
+    sample_min = sorted_arc[-1]
+    sample_median = statistics.median(sorted_arc)
+    sample_mean = statistics.mean(sorted_arc)
+    sample_sd = statistics.stdev(sorted_arc, sample_mean)   # Pass mean to save recalculation
+
+    print("{0:>18} = {1}".format("Sample Size", sample_size))
+    print("{0:>18} = {1}".format("Max", sample_max))
+    print("{0:>18} = {1}".format("Min", sample_min))
+    print("{0:>18} = {1}".format("Median", sample_median))
+    print("{0:>18} = {1}".format("Mean", sample_mean))
+    print("{0:>18} = {1}".format("Standard Deviation", sample_sd))
+
+
+main_archive = read_archive()
+final = find_final_entry(main_archive)
+print("\nScore")
+basic_statistics(final.score)
+print("\nComments")
+basic_statistics(final.comments)
+print("Done")
